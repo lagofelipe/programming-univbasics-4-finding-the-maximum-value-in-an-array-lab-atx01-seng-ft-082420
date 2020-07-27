dbf7cfb88b1527ac70945ee2ccc7927abe982ce4
Felipe Dolago <lagofelipe@gmail.com>
2020-07-27 13:48:04 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -13,18 +13,3 @@ end
 count += 1
 end
 end
-
-
-
-
-
-
-def find_element_index(array, value_to_find)
-count = 0
-while count < array.length do
-  if array[count] == value_to_find
-    return count
-  end
-count += 1
-end
-end
