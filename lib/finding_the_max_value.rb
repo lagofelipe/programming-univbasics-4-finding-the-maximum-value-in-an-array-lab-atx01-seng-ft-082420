ca31567e918d53df2c4aab863ed4eebe094d583b
Felipe Dolago <lagofelipe@gmail.com>
2020-07-27 14:14:57 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,11 @@
 def find_max_value(array)
 count = 0
-maximum_value = nil
+maximum_value = -1
 while count < array.length do
   if array[count] > maximum_value
     maximum_value = array[count]
-    return maximum_value
-end
-
-end
+    end
 count += 1
+end
+return maximum_value
 end
